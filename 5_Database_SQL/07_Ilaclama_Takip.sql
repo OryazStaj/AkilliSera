e526733c2e4cb06770e12229e4ff8eb44cd5fc2c
Hasan Köstek <hasankostek3@gmail.com>
2026-08-11 11:03:49 +0300

    Delete 5_Database_SQL directory

--- 5_Database_SQL/07_Ilaclama_Takip.sql
+++
@@ -1,8 +0,0 @@
-create table Ilaclama_Takip (
-	ilaclama_ID int primary key identity(1,1),
-	hastalik_ID int foreign key references Bitki_Hastalik(hastalik_ID),
-	ilacAdi nvarchar(100),
-	uygulamaZamani datetime default getdate()
-);
-
-select * from Ilaclama_Takip;
