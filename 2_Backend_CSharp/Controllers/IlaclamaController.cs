247a7f30eadbcfb95245978655eec41a46a4d0f6
sudenazduzgun <ilknursudenazduzgun@gmail.com>
2026-08-11 16:31:07 +0300

    Add files via upload

---
+++ 2_Backend_CSharp/Controllers/IlaclamaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AkilliSera_API.Models;
+using AkilliSera_API.Services;
+
+namespace AkilliSera_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IlaclamaController:ControllerBase
+    {
+        private readonly DataBaseService _databaseService;
+
+        public IlaclamaController(DataBaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+        [HttpGet("gecmis")]
+        public IActionResult GetIlaclamaGecmisi()
+        {
+            var veriler = _databaseService.IlaclamaGecmisi();
+            return Ok(veriler);
+        }
+        [HttpPost("ekle")]
+        public IActionResult AddIlaclama([FromBody] IlaclamaTakip yeniIlaclama)
+        {
+            if (yeniIlaclama == null)
+            {
+                return BadRequest("Geçersiz veya boş veri.");
+            }
+
+            _databaseService.IlaclamaEkle(yeniIlaclama);
+            return Ok(new { message = "İlaçlama kaydı başarıyla eklendi." });
+        }
+    }
+}
---
+++ 2_Backend_CSharp/Models/AksiyonLoglari.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkilliSera_API.Models;
+
+public partial class AksiyonLoglari
+{
+    public int LogId { get; set; }
+
+    public int? SeraId { get; set; }
+
+    public string? CihazAdi { get; set; }
+
+    public string? Aksiyon { get; set; }
+
+    public DateTime? KayitZamani { get; set; }
+
+    public virtual SeraDurum? Sera { get; set; }
+}
