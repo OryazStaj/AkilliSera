e6cf8a9eddc132419bf23a20f12f5306c35428f0
yusufhekarli <ysf.hkrl@gmail.com>
2026-08-14 14:58:46 +0300

    Add files via upload

---
+++ 2_Backend_CSharp/Services/HealthCheckService.cs
@@ -0,0 +1,20 @@
+﻿namespace AkıllıSera.API.Services;
+
+public class HealthCheckService
+{
+
+    // Cihazdan alınan en son verinin zamanı
+ 
+    private DateTime _lastSeen = DateTime.MinValue;
+    // Cihazın çevrimiçi olup olmadığını belirlemek için kullanılacak zaman aşımı süresi
+    private readonly TimeSpan _timeout = TimeSpan.FromSeconds(30);
+
+    // Cihazdan alınan en son verinin zamanını güncellemek için kullanılan yöntem
+    public void UpdatePulse() => _lastSeen = DateTime.UtcNow;
+
+    // Cihazın son 30 saniye de çevrimiçi olup olmadığını kontrol etmek için kullanılan yöntem
+    public bool IsOnline() => (DateTime.UtcNow - _lastSeen) < _timeout;
+
+    // Cihazdan alınan en son verinin zamanını almak için kullanılan yöntem(Son görüldü)
+    public DateTime GetLastSeen() => _lastSeen;
+}
