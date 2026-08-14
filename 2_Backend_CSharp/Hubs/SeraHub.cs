042e85bd47a352faeae625d7759ec29da1de0c85
yusufhekarli <ysf.hkrl@gmail.com>
2026-08-14 14:58:20 +0300

    Add files via upload

---
+++ 2_Backend_CSharp/Hubs/SeraHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AkıllıSera.API.Hubs;
+
+/// <summary>
+/// ESP32'den gelen anlık telemetri verilerini Frontend (web/mobil) istemcilerine
+/// canlı olarak yayınlayan SignalR iletişim kanalı (Hub) sınıfı.
+/// </summary>
+public class SeraHub : Hub
+{
+}
