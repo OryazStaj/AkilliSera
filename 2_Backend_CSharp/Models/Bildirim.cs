9c946595a8bede1ada33c8871b20fdae966f3f3a
sudenazduzgun <ilknursudenazduzgun@gmail.com>
2026-08-17 15:28:42 +0300

    Add files via upload

---
+++ 2_Backend_CSharp/Models/Bildirim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkilliSera_API.Models;
+
+public partial class Bildirim
+{
+    public int BildirimId { get; set; }
+
+    public string? Mesaj { get; set; }
+
+    public bool? OkunduBilgisi { get; set; }
+
+    public DateTime? BildirimZamani { get; set; }
+}
